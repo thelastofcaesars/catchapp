46609f773cbe0981a9d0ad782644d4333da89204
Cezary Janiak <cezaryjaniak01@gmail.com>
2020-06-07 01:06:03 +0200

    Added bottom nav bar to start manager

--- lib/src/start.dart
+++ lib/src/start.dart
@@ -10,6 +10,31 @@ class StartPage extends StatefulWidget {
 
 class _StartPageState extends State<StartPage> {
 
+  int _cIndex = 0;
+  
+  static const List<Widget> _widgetOptions = <Widget> [
+    Text(
+      'Index 0',
+    ),
+    Text(
+      'Index 1',
+    ),
+    Text(
+      'Index 2',
+    ),
+    Text(
+      'Index 3',
+    ),
+    Text(
+      'Index 4',
+    ),
+  ];
+  
+  void _onItemTapped(index) {
+    setState(() {
+      _cIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +42,37 @@ class _StartPageState extends State<StartPage> {
         title: Text(widget.title),
       ),
       body: Center(
+        child: _widgetOptions[_cIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map, color: Colors.black,),
+            title: Text('nav', style: TextStyle(color: Colors.black), textScaleFactor: 0,),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.date_range, color: Colors.black,),
+            title: Text('nav', style: TextStyle(color: Colors.black), textScaleFactor: 0,),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite, color: Colors.black,),
+            title: Text('nav', style: TextStyle(color: Colors.black), textScaleFactor: 0,),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search, color: Colors.black,),
+            title: Text('nav', style: TextStyle(color: Colors.black), textScaleFactor: 0,),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings, color: Colors.black,),
+            title: Text('nav', style: TextStyle(color: Colors.black), textScaleFactor: 0,),
+          ),
+        ],
+        currentIndex: _cIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.amber,
+        onTap: (index) {
+          _onItemTapped(index);
+        },
       ),
     );
   }
