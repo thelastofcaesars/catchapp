06293599c104f49cc53fd90b6c1069ca5edbb58d
Cezary Janiak <cezaryjaniak01@gmail.com>
2020-06-07 04:16:59 +0200

    Added full example info

--- lib/src/settings.dart
+++ lib/src/settings.dart
@@ -10,8 +10,10 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
 
   String _username = "RescueR";
-  String _status = "Protester/Bussinessman";
-  
+  String _status = "Protester";
+  String _rank = "Looter";
+  String _sPlaceholder = "Lorem ipsum";
+
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -32,30 +34,71 @@ class _SettingsPageState extends State<SettingsPage> {
                 ),
               ),
               Container(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: ListView(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
                     Text('Username: '),
-                    Text(_username),
+                    Text(_username, style: TextStyle(fontSize: 18),),
+                    Divider(height: 8,),
                     Text('Status: '),
-                    Text(_status),
+                    Text(_status, style: TextStyle(fontSize: 18),),
+                    Divider(height: 8,),
+                    Text('Rank: '),
+                    Text(_rank, style: TextStyle(fontSize: 18),),
                   ],
                 ),
-              )
+              ),
+            ],
+          ),
+        ),
+        Divider(height: 20,),
+        Container(
+          height: 200,
+          color: Colors.grey[200],
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('Email: '),
+              Text(_sPlaceholder, style: TextStyle(fontSize: 18),),
+              Divider(height: 10,),
+              Text('Organisation: '),
+              Text(_sPlaceholder, style: TextStyle(fontSize: 18),),
+              Divider(height: 10,),
+              Text('Phone: '),
+              Text(_sPlaceholder, style: TextStyle(fontSize: 18),),
+              Divider(height: 10,),
+              Text('FAX: '),
+              Text(_sPlaceholder, style: TextStyle(fontSize: 18),),
             ],
           ),
         ),
         Divider(height: 10,),
         Container(
-          height: 50,
+          height: 200,
           color: Colors.grey[200],
-          child: const Center(child: Text('Entry B')),
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('About me: '),
+              Text(_sPlaceholder, style: TextStyle(fontSize: 18),),
+            ],
+          ),
         ),
         Divider(height: 10,),
         Container(
-          height: 50,
-          color: Colors.grey[200],
-          child: const Center(child: Text('Entry C')),
+          height: 60,
+          color: Colors.green[700],
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text('About author: ', style: TextStyle(fontSize: 14, color: Colors.white),),
+              Text('Lone Warrior in the Universe, the Wizard', style: TextStyle(fontSize: 18, color: Colors.white),),
+            ],
+          ),
         ),
       ],
     );
