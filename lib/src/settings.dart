96d147d579f6a6d6ed7e92f989272093a84f9c1b
Cezary Janiak <cezaryjaniak01@gmail.com>
2020-06-07 02:54:09 +0200

    Added sample profile image

--- lib/src/settings.dart
+++ lib/src/settings.dart
@@ -9,6 +9,9 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
 
+  String _username = "RescueR";
+  String _status = "Protester/Bussinessman";
+  
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -16,20 +19,42 @@ class _SettingsPageState extends State<SettingsPage> {
       scrollDirection: Axis.vertical,
       children: <Widget>[
         Container(
-          height: 50,
-          color: Colors.amber[600],
-          child: const Center(child: Text('Entry A')),
+          height: 150,
+          color: Colors.grey[200],
+          child: Row(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Image( // your picture from last HackYeah ;)
+                  height: 128, 
+                  width: 128, 
+                  image: NetworkImage('https://www.shareicon.net/data/512x512/2015/12/04/682309_square_512x512.png'),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: ListView(
+                  children: <Widget>[
+                    Text('Username: '),
+                    Text(_username),
+                    Text('Status: '),
+                    Text(_status),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
         Divider(height: 10,),
         Container(
           height: 50,
-          color: Colors.amber[500],
+          color: Colors.grey[200],
           child: const Center(child: Text('Entry B')),
         ),
         Divider(height: 10,),
         Container(
           height: 50,
-          color: Colors.amber[100],
+          color: Colors.grey[200],
           child: const Center(child: Text('Entry C')),
         ),
       ],
