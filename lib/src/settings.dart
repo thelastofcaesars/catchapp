4acc4500dc4f8a74e9e694e4a84f5370a6db6605
Cezary Janiak <cezaryjaniak01@gmail.com>
2020-06-07 02:21:43 +0200

    Added list view with random content

--- lib/src/settings.dart
+++ lib/src/settings.dart
@@ -11,12 +11,28 @@ class _SettingsPageState extends State<SettingsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'TODO Settings',
+    return ListView(
+      padding: EdgeInsets.all(10),
+      scrollDirection: Axis.vertical,
+      children: <Widget>[
+        Container(
+          height: 50,
+          color: Colors.amber[600],
+          child: const Center(child: Text('Entry A')),
         ),
-      ),
+        Divider(height: 10,),
+        Container(
+          height: 50,
+          color: Colors.amber[500],
+          child: const Center(child: Text('Entry B')),
+        ),
+        Divider(height: 10,),
+        Container(
+          height: 50,
+          color: Colors.amber[100],
+          child: const Center(child: Text('Entry C')),
+        ),
+      ],
     );
   }
 }
